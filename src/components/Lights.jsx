dd80d7b09a7290c92cf03d3d8fc3b4f577ace0e9
Abhishek Gupta <118703472+Abhishek14104@users.noreply.github.com>
2024-07-14 19:12:29 +0530

    Update Lights.jsx

--- src/components/Lights.jsx
+++ src/components/Lights.jsx
@@ -2,18 +2,9 @@ import { Environment, Lightformer } from "@react-three/drei";
 
 const Lights = () => {
   return (
-    // group different lights and lightformers. We can use group to organize lights, cameras, meshes, and other objects in the scene.
     <group name="lights">
-      {/**
-       * @description Environment is used to create a background environment for the scene
-       * https://github.com/pmndrs/drei?tab=readme-ov-file#environment
-       */}
       <Environment resolution={256}>
         <group>
-          {/**
-           * @description Lightformer used to create custom lights with various shapes and properties in a 3D scene.
-           * https://github.com/pmndrs/drei?tab=readme-ov-file#lightformer
-           */}
           <Lightformer
             form="rect"
             intensity={10}
@@ -38,17 +29,12 @@ const Lights = () => {
         </group>
       </Environment>
 
-      {/**
-       * @description spotLight is used to create a light source positioned at a specific point
-       * in the scene that emits light in a specific direction.
-       * https://threejs.org/docs/#api/en/lights/SpotLight
-       */}
       <spotLight
         position={[-2, 10, 5]}
         angle={0.15}
-        penumbra={1} // the penumbra is the soft edge of a shadow cast by a point light
-        decay={0} // the amount the light dims as it moves away from the source
-        intensity={Math.PI * 0.2} // the light intensity
+        penumbra={1} 
+        decay={0} 
+        intensity={Math.PI * 0.2} 
         color={"#f8f9fa"}
       />
       <spotLight
