ed85d764f8a08fbe531cf73551752aba020bca8c
Abhishek Gupta <abhishek14104@gmail.com>
2024-07-14 18:47:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Highlight from './components/Highlight'
+import Hero from './components/Hero'
+import Model from './components/Model'
+import Features from './components/Features'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Highlight />
+      <Model />
+      <Features />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
